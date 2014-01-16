5c712cecb90c5c07afdbee3e16fb3443285d36f0
William Gorge <doubletrap@gorgew.rez>
2014-01-16 23:50:45 +0100

    Version 0.2
    Nouveaux tests qui marchent, notamment celui sur une colonne aléatoire.
    Créé alors les mots processeurs à soumettre

--- BitWeaving/BitWeaving-H/1-Premier_Test/src/BitWeavingH.java
+++ BitWeaving/BitWeaving-H/1-Premier_Test/src/BitWeavingH.java
@@ -16,10 +16,14 @@ public class BitWeavingH {
 		
 		k = size_of_one_data;
 		w = size_of_processor_word;
-		Ls = w/(k+1);
+		int N = w/(k+1);
+		Ls = N*(k+1);
+		int NbFullSegments = col.length/Ls;
+		int rest = col.length % Ls;
 		
 		// Creation of the segment array
-		this.column = new BWH_Segment[col.length/Ls];
+		if(rest == 0) this.column = new BWH_Segment[NbFullSegments]; // Case when Ls divides the length of the column
+		else this.column = new BWH_Segment[NbFullSegments+1];	// Case if not: we have to add another segment
 		
 		// Itterate for all the segments
 		for(int i = 0; i < col.length/Ls; ++i) {
@@ -32,6 +36,18 @@ public class BitWeavingH {
 			
 			// BWH Segment creation (creation of processor words)
 			this.column[i] = new BWH_Segment(segmt, k, w);
+		}
+		
+		// We sould not forget the possible rest of the segmentation: the last segment
+		if(rest > 0) {
+			
+			long [] segmt = new long[rest];
+			for(int j = 0; j < rest; ++j) {
+				segmt[j] = col[NbFullSegments*Ls + j];
+			}
+			
+			// BWH Segment creation (creation of processor words)
+			this.column[NbFullSegments] = new BWH_Segment(segmt, k, w);
 		}
 		
 	}
@@ -44,19 +60,6 @@ public class BitWeavingH {
 	// Set a new column
 	public void setColumn(long[] col) {
 		
-		// Itterate for all the segments
-		for(int i = 0; i < col.length/Ls; ++i) {
-			
-			// Copy from the column to the segment
-			long [] segmt = new long[Ls];
-			for(int j = 0; j < Ls; ++j) {
-				segmt[j] = col[i+j];
-			}
-			
-			// BWH Segment creation (creation of processor words)
-			this.column[i] = new BWH_Segment(segmt, k, w);
-		}
-
 	}
 	
 	public long is_column_less_than(long cst) {
--- BitWeaving/BitWeaving-H/1-Premier_Test/src/MainTestBWH.java
+++ BitWeaving/BitWeaving-H/1-Premier_Test/src/MainTestBWH.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 
 public class MainTestBWH {
 
@@ -6,7 +8,7 @@ public class MainTestBWH {
 		for(int i = 0; i < Long.numberOfLeadingZeros(l); ++i) {
 			s += "0";
 		}
-		s += Long.toBinaryString(l);
+		if(l != 0) s += Long.toBinaryString(l);
 		return s;
 	}
 	
@@ -17,26 +19,126 @@ public class MainTestBWH {
 	public static void main(String[] args) {
 		// TODO Remplir le tableau de zŽros
 		// TODO Faire les requtes test
-		//long[] column = new long[1000];
-		int k = 3;
-		int w = 8;
-		int N = w/(k+1);
-		int Ls = N*(k+1);
-		long[] column_segment = new long[Ls];
 		
+		// DŽfinit de quel test il s'agit
+		int example = 0;
+		
+		
+		
+		/*************** Exemple sur une colonne de chiffres alŽatoires ************/
+		if(example == 0) {
+			int k = 3;
+			int w = 8;
+			int N = w/(k+1);
+			int column_length = 10;
+			
+			long[] column = new long[column_length];
+			
+			System.out.println("Column generated: ");
+			for(int i = 0; i < column.length; ++i) {
+				column[i] = (long) (Math.random()*Math.pow(2, k));
+				System.out.println("	" + longtobyteString(column[i]).substring(64-k));
+			}
+			
+			BitWeavingH BWH = new BitWeavingH(column,k,w);
+			BWH_Segment[] column_out = BWH.getColumn();
+			
+			// Affichage
+			System.out.println("\nProcessor words: ");
+			for(int n = 0; n < column_out.length; ++n) {
+				System.out.println("	Segment" + (n+1));
+				BWH_Segment s = column_out[n];
+				for(int i = 0; i<s.getProcessorWords().length; ++i) {
+					System.out.println("	v" + (i+1)+ ": " + longtobyteString(s.getProcessorWords()[i]).substring(64-N*(k+1)));
+				}
+				System.out.println("");
+			}
+			
+		}
+				
+			
+		/********* Cas des exemples traitŽs dans les slides ***************/
+		// Exemple 1 (k = 3)
+		if(example == 1) {
+			int k = 3;
+			int w = 8;
+			int N = w/(k+1);
+			int Ls = N*(k+1);
 
-		column_segment[0] = 1;
-		column_segment[1] = 5;
-		column_segment[2] = 6;
-		column_segment[3] = 1;
-		column_segment[4] = 6;
-		column_segment[5] = 4;
-		column_segment[6] = 0;
-		column_segment[7] = 1;
-		
-		BWH_Segment s = new BWH_Segment(column_segment, k, w);
-		for(int i = 0; i<s.getProcessorWords().length; ++i) {
-			System.out.println("v" + (i+1)+ ": " + longtobyteString(s.getProcessorWords()[i]).substring(64-8));
+			long[] column_segment = new long[Ls];
+			column_segment[0] = 1;
+			column_segment[1] = 5;
+			column_segment[2] = 6;
+			column_segment[3] = 1;
+			column_segment[4] = 6;
+			column_segment[5] = 4;
+			column_segment[6] = 0;
+			column_segment[7] = 7;
+			BWH_Segment s = new BWH_Segment(column_segment, k, w);
+			
+			// Affichage
+			for(int i = 0; i<s.getProcessorWords().length; ++i) {
+				System.out.println("v" + (i+1)+ ": " + longtobyteString(s.getProcessorWords()[i]).substring(64-8));
+			}
+		}
+			
+		// Exemple 2 (k = 4)
+		if(example == 2) {
+			int k = 4;
+			int w = 8;
+			int N = w/(k+1);
+			int Ls = N*(k+1);
+
+			long[] column_segment = new long[Ls];
+			column_segment[0] = 9;
+			column_segment[1] = 5;
+			column_segment[2] = 8;
+			column_segment[3] = 1;
+			column_segment[4] = 15;
+			BWH_Segment s = new BWH_Segment(column_segment, k, w);
+			
+			// Affichage
+			for(int i = 0; i<s.getProcessorWords().length; ++i) {
+				System.out.println("v" + (i+1)+ ": " + longtobyteString(s.getProcessorWords()[i]).substring(64-8));
+			}
+		}
+		// Example 3 (k = 2)
+		if(example == 3) {
+			int k = 2;
+			int w = 8;
+			int N = w/(k+1);
+			int Ls = N*(k+1);
+
+			long[] column_segment = new long[Ls];
+			column_segment[0] = 0;
+			column_segment[1] = 2;
+			column_segment[2] = 3;
+			column_segment[3] = 0;
+			column_segment[4] = 3;
+			column_segment[5] = 2;
+			BWH_Segment s = new BWH_Segment(column_segment, k, w);
+			
+			// Affichage
+			for(int i = 0; i<s.getProcessorWords().length; ++i) {
+				System.out.println("v" + (i+1)+ ": " + longtobyteString(s.getProcessorWords()[i]).substring(64-8));
+			}
+		}
+		// Exemple 4 (comme example 1 mais avec un sŽgment incomplet (comme le segment 2 de la figure 3)
+		if(example == 4) {
+			int k = 2;
+			int w = 8;
+			int N = w/(k+1);
+			int Ls = 2;
+
+			long[] column_segment = new long[Ls];
+			column_segment[0] = 4;
+			column_segment[1] = 3;
+			BWH_Segment s = new BWH_Segment(column_segment, k, w);
+			
+			// Affichage
+			for(int i = 0; i<s.getProcessorWords().length; ++i) {
+				System.out.println("v" + (i+1)+ ": " + longtobyteString(s.getProcessorWords()[i]).substring(64-8));
+			}
 		}
 
 	}
