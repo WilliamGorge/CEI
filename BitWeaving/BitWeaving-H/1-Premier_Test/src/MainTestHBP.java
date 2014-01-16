0d4090880f63d94542e0d4864e3a6aeb74c1fb52
William Gorge <doubletrap@gorgew.rez>
2014-01-16 15:12:01 +0100

    Avancement du test HBP et ajout des fichiers eclipse ignorés

--- BitWeaving/BitWeaving-H/1-Premier_Test/src/MainTestHBP.java
+++ BitWeaving/BitWeaving-H/1-Premier_Test/src/MainTestHBP.java
@@ -2,11 +2,14 @@
 public class MainTestHBP {
 
 	/**
-	 * @param args
+	 * @param args: Nothing (yet)
+	 * @author William Gorge
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Remplir le tableau de zéros
+		// TODO Faire les requêtes test
+		long[] column = new long[1000];
+		
 	}
 
 }
