5a238bc1443cf1c6db8a6e9dd461af7365388d9c
bvsordet <benoit.sordet@supelec.fr>
2014-02-19 00:25:34 +0100

    Create BitWeavingVInterface.java

---
+++ BitWeaving/BitWeaving-V/BitWeavingVInterface.java
@@ -0,0 +1,9 @@
+
+public interface BitWeavingVInterface 
+{
+	long[] query(Query queryName, long cst);
+	int size();
+	void add(long nb);
+	BWVSegment[] getColumn();
+	long[] complexQuery(String query);
+}
