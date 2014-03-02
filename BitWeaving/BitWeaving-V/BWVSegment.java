122020046fb597ac3ea23001ff6760b58e145607
bvsordet <benoit.sordet@supelec.fr>
2014-03-02 02:51:12 +0100

    Update and rename BWV_Segment.java to BWVSegment.java

--- BitWeaving/BitWeaving-V/BWVSegment.java
+++ BitWeaving/BitWeaving-V/BWVSegment.java
@@ -2,13 +2,15 @@
 public class BWVSegment 
 {
 
-	private long[] word;	 		// Processor words
+	private long[] word;			// Processor words
 	private int k;					// Size of one datum
 	private int w;
 	private int wordItinerator;
 	private int Ls;
 	private long mEq;
+	private long mEq2;
 	private long res;
+	private long res2;
 
 	// Default constructor
 	public BWVSegment() 
@@ -25,6 +27,7 @@ public class BWVSegment
 		k = sizeOfOneDatum;
 		Ls = dataArray.length;
 		res = 0;
+		res2 = 0;
 		w = widthOfWord;
 		wordItinerator = 0;
 		
@@ -35,6 +38,7 @@ public class BWVSegment
 		else
 			mEq = ~0;
 
+		mEq2 = mEq;
 		int i, j;
 		word = new long[k];
 		long mask, move = ( 1L << (k - 1) );
@@ -80,25 +84,45 @@ public class BWVSegment
 			mEq = ~( ( 1L << (w - Ls) ) - 1 );
 		else
 			mEq = ~0;
+		
+		mEq2 = mEq;
+	}
+	
+	public void add(long[] nb)
+	{
+		int i, j, Lss = Ls + nb.length;
+		long mask, move = ( 1L << (k - 1) );
+		
+		for (i = 0; i < k; ++i)
+		{ 	
+			for (j = Ls; j < Lss; ++j)
+			{
+				// On met à 0 tous les bits de dataArray[j] sauf le (k-1-i) ième, et l'on affecte le tout à mask.
+				mask = nb[j-Ls] & move;
+				// On divise par 2 autant qu'il le faut pour que le résultat soit dans le premier bit.
+				mask = (mask != 0) ? 1 : 0;
+				// On déplace ensuite le bit vers l'emplacement approprié dans le but de l'ajouter à v[i].
+				mask <<= (w-j-1);
+				// On ajoute le résultat à v[i].
+				word[i] |= mask;
+			}
+			
+			move >>>= 1;
+		}
+		
+		Ls = Lss;
+		
+		if (Ls < w)
+			mEq = ~( ( 1L << (w - Ls) ) - 1 );
+		else
+			mEq = ~0;
+		
+		mEq2 = mEq;
 	}
 	
 	public long[] getProcessorWords()
 	{
 		return word;
-	}
-	
-	private void display(long result, int length)
-	{
-		long mask;
-
-		for (int j = 0; j < length; ++j)
-		{
-			mask = result & (1L << (w-1-j));
-			mask = (mask != 0) ? 1 : 0;
-			System.out.print(mask);
-		}
-		
-		System.out.println();
 	}
 	
 	public void recharge()
@@ -107,6 +131,15 @@ public class BWVSegment
 		mEq = (Ls < w) ? (1L << Ls) - 1 : ~0;
 		wordItinerator = 0;
 	}	
+	
+	public void recharge2()
+	{
+		res = 0;
+		res2 = 0;
+		mEq = (Ls < w) ? (1L << Ls) - 1 : ~0;
+		mEq2 = mEq;
+		wordItinerator = 0;
+	}
 
 	public long lessThan(long[] cst)
 	{		
@@ -130,6 +163,20 @@ public class BWVSegment
 		}
 
 		return res;
+	}
+	
+	public long between(long[] cst1, long[] cst2)
+	{
+		while ((mEq != 0 || mEq2 != 0) && wordItinerator < k)
+		{
+			res |= ( mEq & (~word[wordItinerator] & cst1[wordItinerator]) );
+			mEq &= ~(cst1[wordItinerator] ^ word[wordItinerator]);
+			res2 |= ( mEq2 & ( ~cst2[wordItinerator] & word[wordItinerator] ) );
+			mEq2 &= ~(cst2[wordItinerator] ^ word[wordItinerator]);
+			++wordItinerator;
+		}
+		
+		return ~(res|res2);
 	}
 
 	public long equalTo(long[] cst)
